01c74b3da12787baaebe8dd74624a4f7829d50ab
Ramin Rakhamimov <ramin32@gmail.com>
2016-03-22 12:15:40 -0400

    cleaned up package.json; added normal submit testcase; added localstorage to testdom

--- test/AssistanceRequests-test.js
+++ test/AssistanceRequests-test.js
@@ -13,4 +13,43 @@ describe('AssistanceRequests', function () {
     var errorFields = TestUtils.scryRenderedDOMComponentsWithClass(requestComponent, 'error-message');
     chai.expect(errorFields.length).to.equal(6);
   });
+
+  it('partial submit', function () {
+    var requestComponent = TestUtils.renderIntoDocument(<AssistanceRequests />);
+    var requestForm = TestUtils.findRenderedDOMComponentWithTag(requestComponent, 'form');
+    var inputFields = TestUtils.scryRenderedDOMComponentsWithTag(requestComponent, 'input');
+    inputFields.forEach(function (f) {
+        TestUtils.Simulate.change(f, {target: {name: f.name, value: 'test'}});
+    });
+
+    TestUtils.Simulate.submit(requestForm);
+    var errorFields = TestUtils.scryRenderedDOMComponentsWithClass(requestComponent, 'error-message');
+    chai.expect(errorFields.length).to.equal(2);
+  });
+
+  it('normal submit', function () {
+    var requestComponent = TestUtils.renderIntoDocument(<AssistanceRequests />);
+    var requestForm = TestUtils.findRenderedDOMComponentWithTag(requestComponent, 'form');
+    var inputFields = TestUtils.scryRenderedDOMComponentsWithTag(requestComponent, 'input');
+    inputFields.forEach(function (f) {
+        if (f.type == "checkbox") {
+            TestUtils.Simulate.change(f, {target: {name: f.name, type: "checkbox", checked: true}});
+        }
+        else {
+            TestUtils.Simulate.change(f, {target: {name: f.name, value: "test"}});
+        }
+    });
+
+    var serviceTypeField = TestUtils.findRenderedDOMComponentWithTag(requestComponent, 'select');
+    TestUtils.Simulate.change(serviceTypeField, {target: {name: serviceTypeField.name, value: 'benefits'}});
+
+    var descriptionField = TestUtils.findRenderedDOMComponentWithTag(requestComponent, 'textarea');
+    TestUtils.Simulate.change(descriptionField, {target: {name: descriptionField.name, value: 'some description...'}});
+
+    TestUtils.Simulate.submit(requestForm);
+    var errorFields = TestUtils.scryRenderedDOMComponentsWithClass(requestComponent, 'error-message');
+    chai.expect(errorFields.length).to.equal(0);
+  });
+
+
 });
--- test/testdom.js
+++ test/testdom.js
@@ -7,6 +7,14 @@ var win = doc.defaultView;
 global.document = doc;
 global.window = win;
 
+global.window.localStorage = {
+    _data       : {},
+    setItem     : function(id, val) { return this._data[id] = String(val); },
+    getItem     : function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
+    removeItem  : function(id) { return delete this._data[id]; },
+    clear       : function() { return this._data = {}; }
+};
+
 propagateToGlobal(win);
 
 function propagateToGlobal (window) {
